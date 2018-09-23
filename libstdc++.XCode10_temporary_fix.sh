5b15a98013444747476212cb3f072fee0dcf0142
BluePandaLi <it20032008@gmail.com>
2018-09-23 16:45:58 +0800

    Init commit.

---
+++ libstdc++.XCode10_temporary_fix.sh
@@ -0,0 +1,15 @@
+PARAMETER=$(echo $1 | tr '[A-Z]' '[a-z]')
+if [[ "$PARAMETER" =~ "uninstall" ]]; then
+	sudo rm -f /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/CoreSimulator/Profiles/Runtimes/iOS.simruntime/Contents/Resources/RuntimeRoot/usr/lib/libstdc++*
+	sudo rm -f /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/usr/lib/libstdc++*
+	sudo rm -f /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/lib/libstdc++*
+	echo "libstdc++ uninstall success."
+elif [[ "$PARAMETER" =~ "install" ]]; then
+	sudo cp ./iPhoneOS.platform/libstdc++* /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/CoreSimulator/Profiles/Runtimes/iOS.simruntime/Contents/Resources/RuntimeRoot/usr/lib/
+	sudo cp ./iPhoneSimulator.platform/libstdc++* /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/usr/lib/
+	sudo cp ./MacOSX.platform/libstdc++* /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/lib
+	echo "libstdc++ install success."	
+else
+	echo "Use -install or -uninstall parameter please."
+	echo "libstdc++ extract by Xcode 9.4."
+fi
